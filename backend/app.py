e707f54d3c263e542b6b5995ca98535469ab3074
Anna Sehgal <152269163+annasehgal@users.noreply.github.com>
2026-07-14 22:29:46 +0000

    update

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
 
@@ -7,9 +8,18 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
+@app.route('/')
 def homepage():
-    return "Backend running"
+    return "CSUB Procurement Assistant API"
+
+@app.route('/chat', methods = ['POST'])
+def chat():
+    data = request.get_json()
+    user_message = request.json.get("message", "")
+
+    return jsonify({
+        "answer": f"Hi I am working: {user_message}"
+    })               
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
